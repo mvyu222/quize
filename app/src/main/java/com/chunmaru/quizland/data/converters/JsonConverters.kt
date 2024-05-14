118c3b3e4d459870d4518a60baf97b583795c6ae
Hoow <ipz222_roi@student.ztu.edu.ua>
2024-05-14 12:39:30 +0300

    removed Converters, added coroutines dispatcher, exported phrases to string resource

--- app/src/main/java/com/chunmaru/quizland/data/converters/JsonConverters.kt
+++ app/src/main/java/com/chunmaru/quizland/data/converters/JsonConverters.kt
@@ -3,19 +3,14 @@ package com.chunmaru.quizland.data.converters
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class JsonConverters {
+object JsonConverters {
 
-    companion object {
-
-        fun fromJson(value: String): List<String> {
-            val listType = object : TypeToken<List<String>>() {}.type
-            return Gson().fromJson(value, listType)
-        }
-
-        fun toJson(values: List<String>): String {
-            return Gson().toJson(values)
-        }
+    fun fromJson(value: String): List<String> {
+        val listType = object : TypeToken<List<String>>() {}.type
+        return Gson().fromJson(value, listType)
     }
 
-
+    fun toJson(values: List<String>): String {
+        return Gson().toJson(values)
+    }
 }
